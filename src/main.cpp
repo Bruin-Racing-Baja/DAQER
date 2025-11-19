42728e078f14584528f8fcfb7f66cec2076f96b1
Meena <annmm537@gmail.com>
2025-11-18 20:55:44 -0800

    update logging

--- src/main.cpp
+++ src/main.cpp
@@ -7,79 +7,75 @@
 #include <TimeLib.h>
 #include <SD.h>
 #include <SPI.h>
-#include <Wire.h>
 
 #define NCIR_ADDR 0x5B
-
 
 uint16_t result;
 float temp = 0.0;
 File logFile;
 char log_name[32];
+
 time_t get_teensy3_time() { return Teensy3Clock.get(); }
 
 void setup() {
   delay(2000);
   Serial.begin(115200);
-
   Wire.begin();
+
+  if(!SD.begin(BUILTIN_SDCARD)) {
+      Serial.println("SD failed!");
+      while(1);
+  }
 
   setSyncProvider(get_teensy3_time);
   bool rtc_set = timeStatus() == timeSet && year() > 2021;
 
   if (!rtc_set) {
-        Serial.println("Warning: Failed to sync time with RTC");
-        logFile = SD.open("log_unknown_time.csv", FILE_WRITE);
-    } else {
-        sprintf(log_name, "log_%04d-%02d-%02d_%02d-%02d-%02d.csv", year(), month(), day(), hour(), minute(), second());
-        logFile = SD.open(log_name, FILE_WRITE);
-    }
-  
-  if(!SD.begin(BUILTIN_SDCARD)) {
-        Serial.println("SD failed!");
-    }
-
-  if (!SD.exists(log_name)) {
-        logFile = SD.open(log_name, FILE_WRITE);
-    if (logFile) {
-        logFile.println("Timestamp,temp");
-        logFile.close();
-    } else {
-        Serial.println("Failed to create new log file");
-    }
+      Serial.println("Warning: Failed to sync time with RTC");
+      strcpy(log_name, "log_unknown_time.csv");
+  } else {
+      sprintf(log_name, "log_%04d-%02d-%02d_%02d-%02d-%02d.csv",
+              year(), month(), day(), hour(), minute(), second());
   }
 
-  
+  if (!SD.exists(log_name)) {
+      logFile = SD.open(log_name, FILE_WRITE);
+      if (logFile) {
+          logFile.println("Timestamp,temp");
+          logFile.close();
+      } else {
+          Serial.println("Failed to create new log file");
+      }
+  }
 }
 
 void loop() {
-
   char timestamp[32];
   unsigned long ms = millis();
-        time_t now = ms / 1000;
-        int ms_part = ms % 1000;
-        sprintf(timestamp, "%04d-%02d-%02d %02d:%02d:%02d.%03d",
-        year(now), month(now), day(now),
-        hour(now), minute(now), second(now), ms_part);
+  time_t now = ms / 1000;
+  int ms_part = ms % 1000;
 
-  
-  logFile = SD.open(log_name, FILE_WRITE);
+  sprintf(timestamp, "%04d-%02d-%02d %02d:%02d:%02d.%03d",
+          year(now), month(now), day(now),
+          hour(now), minute(now), second(now), ms_part);
 
   Wire.beginTransmission(NCIR_ADDR);
   Wire.write(0x07);
   Wire.endTransmission(false);
   Wire.requestFrom(NCIR_ADDR, 2);
+
   result = Wire.read();
   result |= Wire.read() << 8;
-  temp = result * 0.02 - 273.15; 
+  temp = result * 0.02 - 273.15;
 
-  //Serial.println(result);
-  Serial.println(temp); 
+  Serial.println(temp);
 
+  logFile = SD.open(log_name, FILE_WRITE);
   if (logFile) {
-      logFile.print(timestamp); logFile.print(",");
-      logFile.println(temp); 
-      logFile.close();  
+      logFile.print(micros());
+      logFile.print(",");
+      logFile.println(temp);
+      logFile.close();
   } else {
       Serial.println("Failed to write to log");
   }
