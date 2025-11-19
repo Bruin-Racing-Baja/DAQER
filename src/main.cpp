8d1f0f57a06fc1b3f6b3989e31ea37582c666d70
Meena <annmm537@gmail.com>
2025-11-18 19:57:36 -0800

    imu only

--- src/main.cpp
+++ src/main.cpp
@@ -6,77 +6,54 @@
 #include <SPI.h>
 #include <TimeLib.h>
 
-
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3D
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-
-
-
-
-#define SHOCK_1 A2
-#define SHOCK_2 A13
-#define BPS     A12
 #define LED1_PIN 4
 #define LED2_PIN 9
 #define BUFFER_SIZE 50
 
-time_t get_teensy3_time() { return Teensy3Clock.get(); }
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+Adafruit_BNO08x bno08x;
+sh2_SensorValue_t sensorValue;
+
+time_t get_teensy3_time() { return Teensy3Clock.get(); }
+IntervalTimer loggerTimer;
 
 char log_name[32];
 File logFile;
 
-
-IntervalTimer loggerTimer;
-
-
-struct SensorPacket {
-    unsigned long timestamp;
-    int shock1, shock2;
+struct TimePacket {
+    unsigned long ts;
 };
-volatile SensorPacket latestSample;
-volatile bool newSampleReady = false;
 
-// --- ISR: fast, non-blocking ---
+volatile TimePacket buffer[BUFFER_SIZE];
+volatile uint8_t head = 0;
+volatile uint8_t tail = 0;
+
+struct IMUPacket {
+    float ax, ay, az;
+    float qw, qx, qy, qz;
+};
+
+IMUPacket latestIMU;
+
 void logger_function() {
-    latestSample.timestamp = micros();
-    //Serial.println(latestSample.timestamp);
-    
-    latestSample.shock1 = analogRead(SHOCK_1);
-    latestSample.shock2 = analogRead(SHOCK_2);
-
-    newSampleReady = true;
+    uint8_t next = (head + 1) % BUFFER_SIZE;
+    if (next == tail) return;
+    buffer[head].ts = micros();
+    head = next;
 }
-
 
 void setup() {
     Serial.begin(115200);
-    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+
     setSyncProvider(get_teensy3_time);
     bool rtc_set = timeStatus() == timeSet && year() > 2021;
-    if (!rtc_set) {
-        Serial.println("Warning: Failed to sync time with RTC");
-        logFile = SD.open("log_unknown_time.csv", FILE_WRITE);
-    } else {
-        sprintf(log_name, "log_%04d-%02d-%02d_%02d-%02d-%02d.csv", year(), month(), day(), hour(), minute(), second());
-        logFile = SD.open(log_name, FILE_WRITE);
-    }
- 
-    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-        while (true);
-    }
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(10, 20);
-    display.println("Initializing...");
-    display.display();
 
-    // Initialize SD
     if (!SD.begin(BUILTIN_SDCARD)) {
         while (true) {
             digitalWrite(LED1_PIN, HIGH);
@@ -86,18 +63,21 @@ void setup() {
         }
     }
 
-    // Create log file with timestamp
-    // sprintf(log_name, "log_%04d-%02d-%02d_%02d-%02d-%02d.csv",
-    //         year(), month(), day(), hour(), minute(), second());
-    //logFile = SD.open(log_name, FILE_WRITE);
+    if (!rtc_set) {
+        strcpy(log_name, "log_unknown_time.csv");
+    } else {
+        sprintf(log_name, "log_%04d-%02d-%02d_%02d-%02d-%02d.csv",
+                year(), month(), day(), hour(), minute(), second());
+    }
+
     if (!SD.exists(log_name)) {
-        logFile = SD.open(log_name, FILE_WRITE);
-        if (logFile) {
-            logFile.println("Timestamp,ShockPot1(mm),ShockPot2(mm)");
-            logFile.close();
+        File f = SD.open(log_name, FILE_WRITE);
+        if (f) {
+            f.println("timestamp,ax,ay,az,qw,qx,qy,qz");
+            f.close();
         } else {
             Serial.println("Failed to create new log file");
-            while(true){
+            while (true) {
                 digitalWrite(LED2_PIN, HIGH);
                 delay(250);
                 digitalWrite(LED2_PIN, LOW);
@@ -106,44 +86,58 @@ void setup() {
         }
     }
 
-    // Start timer at 200 Hz → 5000 µs
-    loggerTimer.priority(255); // highest priority
+    logFile = SD.open(log_name, FILE_WRITE);
+    if (!logFile) {
+        Serial.println("Failed to open log file for writing");
+        while (true);
+    }
+
+    Wire2.begin();
+    bno08x.begin_I2C(0x4A, &Wire2);
+    bno08x.enableReport(SH2_CAL_ACCEL, 2500);
+    bno08x.enableReport(SH2_ROTATION_VECTOR, 2500);
+
     loggerTimer.begin(logger_function, 5000);
+    loggerTimer.priority(255);
 }
 
-
 void loop() {
-    if (newSampleReady) {
-        
-        newSampleReady = false;
-
-        // Convert analog to units
-        float distance1 = (latestSample.shock1 / 4095.0) * 250.0;
-        float distance2 = (latestSample.shock2 / 4095.0) * 250.0;
-        
-
-        // Log to SD
-        File logFile = SD.open(log_name, FILE_WRITE);
-        if (logFile) {
-            logFile.print(latestSample.timestamp); logFile.print(",");
-
-            logFile.print(distance1); logFile.print(",");
-            logFile.println(distance2); 
-            logFile.close();
+    if (bno08x.getSensorEvent(&sensorValue)) {
+        switch (sensorValue.sensorId) {
+            case SH2_CAL_ACCEL:
+                latestIMU.ax = sensorValue.un.accelerometer.x;
+                latestIMU.ay = sensorValue.un.accelerometer.y;
+                latestIMU.az = sensorValue.un.accelerometer.z;
+                break;
+            case SH2_ROTATION_VECTOR:
+                latestIMU.qw = sensorValue.un.rotationVector.real;
+                latestIMU.qx = sensorValue.un.rotationVector.i;
+                latestIMU.qy = sensorValue.un.rotationVector.j;
+                latestIMU.qz = sensorValue.un.rotationVector.k;
+                break;
         }
+    }
 
-        
-        static unsigned long lastOLED = 0;
-        if (millis() - lastOLED > 50) {
-            lastOLED = millis();
-            display.clearDisplay();
-            display.setCursor(0, 0);
-            display.print("s1: "); display.println(latestSample.shock1);
-            display.print("s2: "); display.println(latestSample.shock2);
-            display.display();
-        }
+    while (tail != head) {
+        TimePacket pkt;
+        noInterrupts();
+        pkt.ts = buffer[tail].ts;
+        tail = (tail + 1) % BUFFER_SIZE;
+        interrupts();
 
-        Serial.print("s1: "); Serial.println(latestSample.shock1);
-        Serial.print("s2: "); Serial.println(latestSample.shock2);
+        logFile.print(pkt.ts); logFile.print(",");
+        logFile.print(latestIMU.ax); logFile.print(",");
+        logFile.print(latestIMU.ay); logFile.print(",");
+        logFile.print(latestIMU.az); logFile.print(",");
+        logFile.print(latestIMU.qw); logFile.print(",");
+        logFile.print(latestIMU.qx); logFile.print(",");
+        logFile.print(latestIMU.qy); logFile.print(",");
+        logFile.println(latestIMU.qz);
+    }
+
+    static uint32_t lastFlush = 0;
+    if (millis() - lastFlush > 100) {
+        lastFlush = millis();
+        logFile.flush();
     }
 }
